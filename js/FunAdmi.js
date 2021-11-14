067394118417f397f09b4a2d76145488969a83f5
DanielMariscal <mariscaldaniel@gmail.com>
2021-11-14 18:37:28 -0400

    App version 6.0

--- js/FunAdmi.js
+++ js/FunAdmi.js
@@ -5,11 +5,11 @@ var nom=localStorage.getItem("Nombre");
 var CerrarSesion=document.getElementById("CerrarSesion");
 CerrarSesion.onclick=function(){
     localStorage. clear();
-    setTimeout(()=>{location.href="../public/index.html";},2000); 
+    setTimeout(()=>{location.href="../index.html";},2000); 
 }
 var Logo=document.getElementsByClassName("logo");
 Logo[0].onclick=function(){
     
-    location.href="../public/indexAdmonistrador.html";
+    location.href="../public/IndexAdmonistrador.html";
 }
 
